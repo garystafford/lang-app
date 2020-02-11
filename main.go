eb0bbd2463319747614d8ada6197d79177864304
Gary Stafford <gary.a.stafford@gmail.com>
2020-02-11 12:28:45 -0800

    Refactor app for API key and logrus logging.

--- main.go
+++ main.go
@@ -9,9 +9,12 @@ package main
 import (
 	"encoding/json"
 	"github.com/rylans/getlang"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/text/language"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -26,7 +29,17 @@ type Info struct {
 
 var (
 	serverPort = ":" + getEnv("LANG_PORT", "8080")
+	apiKey     = getEnv("API_KEY", "")
+	log        = logrus.New()
 )
+
+func init() {
+	log.Formatter = &logrus.JSONFormatter{
+		TimestampFormat: time.RFC3339Nano,
+	}
+	log.Out = os.Stdout
+	log.SetLevel(logrus.DebugLevel)
+}
 
 func main() {
 	// Echo instance
@@ -36,17 +49,19 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	//e.Use(middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
-	//	Skipper: func(c echo.Context) bool {
-	//		if strings.HasPrefix(c.Request().RequestURI, "/health") {
-	//			return true
-	//		}
-	//		return false
-	//	},
-	//	Validator: func(key string, c echo.Context) (bool, error) {
-	//		return key == os.Getenv("AUTH_KEY"), nil
-	//	},
-	//}))
+	e.Use(middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
+		KeyLookup: "header:X-API-Key",
+		Skipper: func(c echo.Context) bool {
+			if strings.HasPrefix(c.Request().RequestURI, "/health") {
+				return true
+			}
+			return false
+		},
+		Validator: func(key string, c echo.Context) (bool, error) {
+			log.Debugf("API_KEY: %v", apiKey)
+			return key == apiKey, nil
+		},
+	}))
 
 	// Routes
 	e.GET("/health", getHealth)
@@ -60,6 +75,7 @@ func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
 	}
+
 	return fallback
 }
 
@@ -67,7 +83,7 @@ func getHealth(c echo.Context) error {
 	var response interface{}
 	err := json.Unmarshal([]byte(`{"status":"UP"}`), &response)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError)
+		return c.JSON(http.StatusInternalServerError, err)
 	}
 
 	return c.JSON(http.StatusOK, response)
@@ -78,7 +94,7 @@ func getLanguage(c echo.Context) error {
 	jsonMap := make(map[string]interface{})
 	err := json.NewDecoder(c.Request().Body).Decode(&jsonMap)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, nil)
+		return c.JSON(http.StatusInternalServerError, err)
 	} else {
 		text := jsonMap["text"]
 		langInfo := getlang.FromString(text.(string))
